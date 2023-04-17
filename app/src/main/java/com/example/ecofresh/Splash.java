0afa2b4a4dda9e3438ea7dc98338d052807bc9e7
Daniel Rodriguez <danicarou.dev@gmail.com>
2023-04-17 15:16:12 +0200

    Mejora del splash

--- app/src/main/java/com/example/ecofresh/Splash.java
+++ app/src/main/java/com/example/ecofresh/Splash.java
@@ -3,6 +3,10 @@ package com.example.ecofresh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+
+
+        // Esta es la parte de la entrada del titulo ECO
+
+        TextView titulo_eco = (TextView) findViewById(R.id.titulo_eco);
+        Animation animTitulo_eco = AnimationUtils.loadAnimation(this, R.anim.entrada_letra_eco);
+        titulo_eco.startAnimation(animTitulo_eco);
+
+
+        // Esta es la parte de la entrada del titulo FRESH
+
+        TextView titulo_fresh = (TextView) findViewById(R.id.titulo_fresh);
+        Animation animTitulo_fresh = AnimationUtils.loadAnimation(this, R.anim.entrada_letra_fresh);
+        titulo_fresh.startAnimation(animTitulo_fresh);
+
+
+        // Esta es la parte del salto de la manzana
+
+        ImageView aple = (ImageView) findViewById(R.id.aple);
+        Animation animAple = AnimationUtils.loadAnimation(this, R.anim.salto_manzana);
+        aple.startAnimation(animAple);
+
     }
 }
