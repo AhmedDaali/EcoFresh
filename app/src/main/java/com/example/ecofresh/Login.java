f0c40ffac82d821635e4e7f3f21c296920a406d8
Daniel Rodriguez <danicarou.dev@gmail.com>
2023-04-20 12:38:02 +0200

    Creada la activity_login, cambio de nombre de la primera activity_login por activity_cliente, modificado el splash y pendiente de comprobar su visualización y funcionamiento correcto

--- app/src/main/java/com/example/ecofresh/Login.java
+++ app/src/main/java/com/example/ecofresh/Login.java
@@ -9,7 +9,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_cliente);
 
 
         // Con esta linea ocultamos el actionBar, la barra de acción situada arriba de todo
