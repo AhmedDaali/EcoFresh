0a327b3110272ab656ccfae0d1bcaa940a41ba70
Daniel Rodriguez <danicarou.dev@gmail.com>
2023-04-20 13:57:12 +0200

    He vuelto a modificar el splash

--- app/src/main/java/com/example/ecofresh/Cliente.java
+++ app/src/main/java/com/example/ecofresh/Cliente.java
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class registro extends AppCompatActivity {
+public class Cliente extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro);
+        setContentView(R.layout.activity_cliente);
+
 
         // Con esta linea ocultamos el actionBar, la barra de acción situada arriba de todo
 
         getSupportActionBar().hide();
+
+
+
+
+
 
     }
 }
