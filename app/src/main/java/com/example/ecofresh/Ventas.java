8c0ca6190d0c7eb8d32da5d93a8a7a30f9910f28
DaniRCarou <danicarou.dev@gmail.com>
2023-05-21 19:47:20 +0200

    Activity Ventas

--- app/src/main/java/com/example/ecofresh/Ventas.java
+++ app/src/main/java/com/example/ecofresh/Ventas.java
@@ -3,12 +3,64 @@ package com.example.ecofresh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ventas extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseFirestore db;
+    String emailUsuario;
+
+    ListView ListviewVentas;
+    List<String> listaVentas = new ArrayList<>();
+    List<String> listaIdVentas = new ArrayList<>();
+    ArrayAdapter<String> mAdapterVentas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventas);
+
+        // Aquí inicializo las instancias de Firebase
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        emailUsuario = mAuth.getCurrentUser().getEmail();
+        ListviewVentas = findViewById(R.id.listViewVentas);
+
+        // Una vez que entra el usuario a esta activity debemos actualizar la interfaz de usuario con sus propias ventas, del usuario logueado
+
+        actualizarUI();
+
+
+
+
+    }
+
+    private void actualizarUI() {
+
+        db.collection("Ventas")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
     }
 }
