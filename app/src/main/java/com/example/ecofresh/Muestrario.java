4bbf30a188c21633563a45dd2952f3eb09e29ce5
AhmedDaali <ahmeddaali2@gmail.com>
2023-05-29 21:11:25 +0200

    Intento sacar foto en productos

--- app/src/main/java/com/example/ecofresh/Muestrario.java
+++ app/src/main/java/com/example/ecofresh/Muestrario.java
@@ -7,9 +7,11 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,6 +20,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,10 +34,12 @@ public class Muestrario extends AppCompatActivity {
     String emailUsuario;
 
     ListView listViewProductos;
-    List<String> listaCantidad = new ArrayList<>();
+    List<String> listaImagenes = new ArrayList<>();
 
     List<String> listaIdProductos = new ArrayList<>();
     ArrayAdapter<String> mAdapterProductos;
+
+    private ImageView imageProducto;
 
     @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
@@ -45,15 +50,20 @@ public class Muestrario extends AppCompatActivity {
         // Obtener los datos de la venta de la venta del Intent.
         String nombreProducto = getIntent().getStringExtra("producto");
 
-        // Con esta linea ocultamos el actionBar, la barra de acción situada arriba de todo
+        //Inicializamos la imágen
+        imageProducto = findViewById(R.id.imageProducto);
 
+        listViewProductos = findViewById(R.id.listViewProductos);
+        listViewProductos.setAdapter(mAdapterProductos);
+
+        // Con esta linea ocultamos el actionBar, la barra de acción situada arriba de todo
         getSupportActionBar().hide();
 
 
 
         // 1 Guardamos la referencia del botón de regreso
-
         botonRegreso = findViewById(R.id.volver);
+
 
         botonRegreso.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,7 +78,6 @@ public class Muestrario extends AppCompatActivity {
         });
 
 
-
         // Aquí inicializo las instancias de Firebase
 
         db = FirebaseFirestore.getInstance();
@@ -77,20 +86,51 @@ public class Muestrario extends AppCompatActivity {
         listViewProductos = findViewById(R.id.listViewProductos);
 
 
-
         // Una vez que entra el usuario a esta activity debemos actualizar la interfaz de usuario con sus propias ventas, del usuario logueado
 
         actualizarUI( nombreProducto);
-
-
-
-
 
 
     }
 
 
     private void actualizarUI(String nombreProducto) {
+       /* mAdapterProductos = new ArrayAdapter<String>(Muestrario.this, R.layout.item_muestrario, R.id.imageProducto, listaImagenes ) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                ImageView imageProducto = view.findViewById(R.id.imageProducto);
+
+                db.collection("VentasRealizadas")
+                        .whereEqualTo("producto.nombre", nombreProducto)
+                        .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                            @Override
+                            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                                if (error != null) {
+                                    // Manejar el error aquí
+                                    return;
+                                }
+
+                                for (QueryDocumentSnapshot doc : value) {
+                                    // Obtener la URL de la imagen para el elemento actual en la posición 'position'
+                                    String imageUrl = doc.getString("producto.photoUrls");
+
+                                    if (imageUrl != null && !imageUrl.isEmpty()) {
+                                        Picasso.get().load(imageUrl).into(imageProducto);
+                                    } else {
+                                        // Si no hay una URL de imagen válida, puedes mostrar una imagen de relleno o dejarla vacía.
+                                        // Por ejemplo:
+                                        // imageProducto.setImageResource(R.drawable.placeholder_image);
+                                        // O simplemente:
+                                        // imageProducto.setImageDrawable(null);
+                                    }
+                                }
+                            }
+                        });
+
+                return view;
+            }
+        };*/
 
         db.collection("VentasRealizadas")
                 .whereEqualTo("producto.nombre", nombreProducto)
@@ -103,9 +143,14 @@ public class Muestrario extends AppCompatActivity {
                             return;
                         }
 
+                        listaImagenes.clear(); // Limpiar la lista de imágenes antes de agregar las nuevas
+
+
                         List<String> listaVentas = new ArrayList<>();
                         for (QueryDocumentSnapshot doc : value) {
                             listaIdProductos.add(doc.getId());
+
+
 
                             String cantidad = doc.getString("cantidad");
                             String vendedor = doc.getString("vendedor");
@@ -132,7 +177,23 @@ public class Muestrario extends AppCompatActivity {
                             mAdapterProductos = new ArrayAdapter<>(Muestrario.this, R.layout.item_muestrario, R.id.textViewProducto, listaVentas);
                             listViewProductos.setAdapter(mAdapterProductos);
                         }
+                        for (QueryDocumentSnapshot doc : value) {
+
+
+                            String imageUrl = doc.getString("producto.photoUrls");
+                            if (imageUrl != null && !imageUrl.isEmpty()) {
+                                listaImagenes.add(imageUrl);
+                                Picasso.get().load(imageUrl).into(imageProducto);
+                            } else {
+                                // Si no hay una URL de imagen válida, puedes mostrar una imagen de relleno o dejarla vacía.
+                                // Por ejemplo:
+                                //listaImagenes.add(""); // Agrega una cadena vacía
+                            }
+                        }
+
+                        //mAdapterProductos.notifyDataSetChanged(); // Notificar al adaptador que los datos han cambiado
                     }
+
                 });
 
     }
